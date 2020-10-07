42e5e5e009028ea780a6bb9f1de919dcc6a642f7
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-10-06 23:02:43 -0400

    comit

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ function onHttpStart() {
   // setup a 'route' to listen on the default url path (http://localhost)
   app.get("/", function(req,res){
       res.send("Hello home<br /><a href='/Home'>Go to the home page</a>");
-  
+      res.send("Hello Listings<br /><a href='/Listings'>Go to the home page</a>");
+      res.send("Hello Registrations<br /><a href='/Registration'>Go to the home page</a>");
+
   });
   
   // setup another route to listen on /about
@@ -20,5 +22,13 @@ function onHttpStart() {
       res.sendFile(path.join(__dirname,"/views/Home.html"));
   });
   
+  app.get("/Home", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/Listings.html"));
+});
+
+app.get("/Home", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/Registration.html"));
+});
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
