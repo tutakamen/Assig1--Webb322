aaccdbb7c3d447851ec9ede59a1a029254cf0a88
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-10-11 10:50:02 -0400

    yes

--- views/myscripts.js
+++ views/myscripts.js
@@ -1,97 +1,26 @@
-function togglePopup1(){
-    document.getElementById("popup-1").classList.toggle("active"); 
-    document.getElementById("d1").innerHTML = "Where are you? Not where you want to be. Not just yet. " ;
-    document.getElementById("d2").innerHTML = "Browse through the properties listed here to find that special place that tickles your fancy." ;
-    document.getElementById("d3").innerHTML = "Our listings our homeowners opening their doors and hearts." ;
-    document.getElementById("d4").innerHTML = "So why book an overpriced unoriginal hotel room ?  Explore the world, feel at home. " ;
+
+var modal = document.getElementById('id01');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var modal = document.getElementById('bg-modal');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+document.getElementById("d1").innerHTML = "Where are you? Not where you want to be. Not just yet. " ;
+document.getElementById("d2").innerHTML = "Browse through the properties listed here to find that special place that tickles your fancy." ;
+document.getElementById("d3").innerHTML = "Our listings our homeowners opening their doors and hearts." ;
+document.getElementById("d4").innerHTML = "So why book an overpriced unoriginal hotel room ?  Explore the world, feel at home. " ;
 
 }
 
-// var br = document.createElement("br");  
-// var down = document.getElementById("a1"); 
-
-// function togglePopup2(){
-
-// document.getElementById("popup-2").classList.toggle("active");  
-// document.getElementById("a1").innerHTML = "Registration Details" ;
-
-//     // Create a form synamically 
-//     var form = document.createElement("form"); 
-//     form.setAttribute("method", "post"); 
-//     form.setAttribute("action", "submit.php"); 
-  
-//     // Create an input element for Full Name 
-//     var FN = document.createElement("input"); 
-//     FN.setAttribute("type", "text"); 
-//     FN.setAttribute("name", "FullName"); 
-//     FN.setAttribute("placeholder", "Full Name"); 
-  
-//      // Create an input element for date of birth 
-//      var DOB = document.createElement("input"); 
-//      DOB.setAttribute("type", "text"); 
-//      DOB.setAttribute("name", "dob"); 
-//      DOB.setAttribute("placeholder", "DOB"); 
-  
-//      // Create an input element for emailID 
-//      var EID = document.createElement("input"); 
-//      EID.setAttribute("type", "text"); 
-//      EID.setAttribute("name", "emailID"); 
-//      EID.setAttribute("placeholder", "E-Mail ID"); 
-  
-//       // Create an input element for password 
-//       var PWD = document.createElement("input"); 
-//       PWD.setAttribute("type", "password"); 
-//       PWD.setAttribute("name", "password"); 
-//       PWD.setAttribute("placeholder", "Password"); 
-  
-//        // Create an input element for retype-password 
-//        var RPWD = document.createElement("input"); 
-//        RPWD.setAttribute("type", "password"); 
-//        RPWD.setAttribute("name", "reTypePassword"); 
-//        RPWD.setAttribute("placeholder", "ReEnter Password"); 
-  
-//                 // create a submit button 
-//                 var s = document.createElement("input"); 
-//                 s.setAttribute("type", "submit"); 
-//                 s.setAttribute("value", "Submit"); 
-                  
-//                 // Append the full name input to the form 
-//                 form.appendChild(FN);  
-                  
-//                 // Inserting a line break 
-//                 form.appendChild(br.cloneNode());  
-                  
-//                 // Append the DOB to the form 
-//                 form.appendChild(DOB);  
-//                 form.appendChild(br.cloneNode());  
-                  
-//                 // Append the emailID to the form 
-//                 form.appendChild(EID);  
-//                 form.appendChild(br.cloneNode());  
-                  
-//                 // Append the Password to the form 
-//                 form.appendChild(PWD);  
-//                 form.appendChild(br.cloneNode());  
-                  
-//                 // Append the ReEnterPassword to the form 
-//                 form.appendChild(RPWD);  
-//                 form.appendChild(br.cloneNode());  
-                  
-//                 // Append the submit button to the form 
-//                 form.appendChild(s);  
-  
-//                 document.getElementsByTagName("div")[0] 
-//                .appendChild(form); 
-            
 
 
-
-// }
-
-
-
-
-
-// window.onload = function() {
-
-// }
