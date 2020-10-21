95c9394ae3b647ab8f6195ac9ef2cd7cbd155486
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-10-21 18:12:30 -0400

    y

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
 
-
-
+var HTTP_PORT = process.env.PORT || 8080;
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
   }
 
-
-//??
 app.use(express.static("views"));
-// app.use(express.static("static")); < ---- Use this instead  ? 
-// app.use('/static', express.static(path.join(__dirname, 'views')));
-
   // setup a 'route' to listen on the default url path (http://localhost)
-  app.get("/", function(req,res){
+  app.get("/Home", function(req,res){
     res.sendFile(path.join(__dirname,"/views/Home.html"));
   });
   
