be6b318f2befd7b5d4bd06bd8ec26bcd383f8cfd
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-10-21 17:34:34 -0400

    y

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 //??
-app.use('/', express.static(path.join(__dirname, 'views')));
+// app.use('/', express.static(path.join(__dirname, 'views')));
 app.use(express.static("views"));
 // 
 
