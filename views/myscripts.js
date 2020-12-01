45c17d05edac46b004c4c090413e9e550b4e2bee
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-11-30 22:20:04 -0500

    y

--- views/myscripts.js
+++ views/myscripts.js
@@ -16,17 +16,7 @@ window.onclick = function(event) {
 }
 var modal = document.getElementById('bg-modal');
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-document.getElementById("d1").innerHTML = "Where are you? Not where you want to be. Not just yet. " ;
-document.getElementById("d2").innerHTML = "Browse through the properties listed here to find that special place that tickles your fancy." ;
-document.getElementById("d3").innerHTML = "Our listings our homeowners opening their doors and hearts." ;
-document.getElementById("d4").innerHTML = "So why book an overpriced unoriginal hotel room ?  Explore the world, feel at home. " ;
 
-}
 //Form Validation---LOGIN FORM
 function validateFormEmail() {
     var x = document.forms["signUp"]["email"].value;
