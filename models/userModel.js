a99c731b86c19f6c945ef61f77cb95b92c076cb7
Mustafa Bukhari <mustafabukhari99@gmail.com>
2020-12-04 20:22:57 -0500

    y

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 mongoose.Promise = require("bluebird");//MUST INSTALL BLUEBIRD  - 1:20  11/10
 
 var UserSchema = new Schema({
+    "admin": Boolean,
     "fname": String,
     "lname": String,
     "email": String,
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ In the video you zoomed the bwoser to make it look like it was working where it 
 Your form processing does not work it does not post 
 correctly and therefore I could not test the email nor the dashboard features
 */
+
+/* #region REQUIRES */
 var express = require("express"); 
 var app = express();
 var path = require("path");
@@ -15,6 +17,11 @@ var  mongoose  = require("mongoose")  ;
 var  Schema  = mongoose.Schema  ; 
 var UserModel = require("./models/userModel"); 
 const config  = require("./js/config"); 
+/* #endregion */
+
+
+/* #region mongoose_connections */
+
 
 var db = mongoose.connect( config.dbconn  , { useNewUrlParser: true ,useUnifiedTopology: true } ) ; //can remove db 
 
@@ -23,6 +30,8 @@ mongoose.Promise = require("bluebird");
 mongoose.connection.on("open",()=>{
     console.log("Working");
 });
+
+/* #endregion */
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -41,6 +50,25 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+//creating user, must replace this with  a find mongodb once works 
+const user = {
+  Admin: true  , 
+  fname : "Mustafa",
+  lname : "Bukhari",
+  password: "Winter22",
+  email: "mustafabukhari99@gmail.com"
+}
+
+function checkLogin(req,res,next){  //must add admin logic 
+  if(!req.session.user){
+    res.redirect("/dashboard");
+  } else {
+    next(); 
+  }
+}
+
+
+/* #region app.get_Engines */
 
 app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -54,22 +82,6 @@ app.use(clientSessions({
   duration: 2*60*1000, 
   activeDuration: 1000*60
 })); 
-
-//creating user, must replace this with  a find mongodb once works 
-const user = {
-  username:"mustafabukhari99@gmail.com",
-  password: "Winter22",
-  email: "mustafabukhari99@gmail.com"
-}
-
-function checkLogin(req,res,next){  //must add admin logic 
-  if(!req.session.user){
-    res.redirect("/login");
-  } else {
-    next(); 
-  }
-}
-
 
 // setup a 'route' to listen on the default url path 
 app.get("/", (req,res) => {
@@ -92,9 +104,44 @@ app.get("/contact", function(req,res){
 });
 
 app.get("/login", function(req,res){
-  res.render('login',{user:req.session.user, layout:false});
+  res.render('login',{ layout:false});
 });
 
+app.post("/login",upload.none(), (req,res)=>{
+  
+  const email =  req.body.email ; 
+  const password =  req.body.password ; 
+  //serverside validation
+  if(email===""|| password === ""){
+    return res.render("login",{ errorMsg: "Somethings missing, or put in incorrectly" , layout:false });  //already done this on hbs page
+  }
+
+  UserModel.findOne({email: email})
+  .exec()// turns this into a promise
+  .then((usr) => {
+      if (!usr) {
+          res.render("login", {errorMsg: "login does not exist!", layout: false});
+      } else {
+          // user exists
+          if (password === usr.password){
+              req.session.user = {//creating session variable 
+                  email: usr.email,
+                  firstName: usr.fname,
+                  lastName: usr.lname,
+                  isAdmin: usr.admin
+              };
+              res.redirect("/dashboard");
+          } else {
+              res.render("login", {errorMsg: "login and password does not match!", layout: false});
+          };
+      };
+
+  })
+  .catch((err) => { console.log("An error occurred: ${err}")});
+
+
+
+});
 
 app.get("/dashboard", checkLogin ,function(req,res){
   res.render('dashboard',{
@@ -102,26 +149,44 @@ app.get("/dashboard", checkLogin ,function(req,res){
     layout:false});
 });
 
-app.post("/login",upload.none(), (req,res)=>{
-  
-  const username =  req.body.email ; 
-  const password =  req.body.password ; 
-  //serverside validation
-  if(username===""|| password === ""){
-    return res.render("login",{ errorMsg: "Both fields are required" , user:req.session.user, layout:false });  //already done this on hbs page
-  }
+app.get("/logout", (req,res)=>{
+  req.session.reset(); 
+  res.redirect("/");
+}); 
 
-  if(username === user.username && password === user.password ){
-    req.session.user = {
-      username:user.username,
-      email:user.email
-    };
-    return res.redirect("/dashboard"); 
-  }
-  else{
-    res.render("login", {errorMsg: "login and password dont match",user:req.session.user,layout:false});
-  }
-});
+app.get("/firstrunsetup", (req,res)=> {
+  var Administrator = new UserModel({
+      admin: true, 
+      fname : "Mustafa",
+      lname : "Bukhari",
+      password: "Winter22",
+      email: "mustafabukhari99@gmail.com"
+    });
+  console.log("got here!");
+  Administrator.save((err)=> {
+      console.log("Error: " + err + ';');
+      if (err) {
+          console.log("There was an error creating Clint: " + err);
+      } else {
+          console.log("Admin is created");
+      }
+  });
+  console.log("got here 2!");
+  res.redirect("/");
+})
+
+
+
+/* #endregion */
+
+/* #region app.post_Engines */
+
+
+
+
+
+
+
 
 
 //this should be the name tag on the photo upload input tag in form  
@@ -132,7 +197,7 @@ app.post("/contact-form-process",upload.none(), (req,res)=> {
       from: 'webb322assigment2@gmail.com',
       to: FORM_DATA.email ,    
       subject: 'Welcome to StayAnywhere',
-      html: `Hello ${req.body.fname}, welcome to StayAnywhere! `
+      html: `Hello ${FORM_DATA.fname}, welcome to StayAnywhere!`
     }
     
       transporter.sendMail(mailOptions,(error,info) =>{
@@ -144,6 +209,7 @@ app.post("/contact-form-process",upload.none(), (req,res)=> {
       }); 
 
       var incomingData  = new UserModel({
+        admin: false,
         fname: req.body.fname,
         lname:  req.body.lname,
         email :  req.body.email,
@@ -157,19 +223,59 @@ app.post("/contact-form-process",upload.none(), (req,res)=> {
           console.log("saved to the web322e collection");
       }   
     });
-    //mongoose.disconnect;  //try adding this :06 class11/26   
-      res.render('dashboard',{
-        data: FORM_DATA, //try change data to user
+
+    res.render('dashboard',{
+        user: FORM_DATA, //try change data to user
         layout:false
       });
 
 });
 
+/* #endregion  */
 
-app.get("/logout", (req,res)=>{
-  req.session.reset(); 
-  res.redirect("/");
-}); 
+    /* #region PROFILES */
+    app.get("/Profile", checkLogin, (req,res)=>{
+      res.render("Profile", {user: req.session.user, layout: false});
+   });
+   
+   app.get("/Profile/Edit", ensureLogin, (req,res)=>{
+       res.render("ProfileEdit", {user: req.session.user, layout: false});
+   });
+   
+   app.post("/Profile/Edit", ensureLogin, (req,res) => {
+       const firstName = req.body.fname;
+       const lastName = req.body.lastname;
+       const Email = req.body.email;
+       const admin = (req.body.admin === "on");
+       UserModel.updateOne(
+           { email: Email },
+           {$set: {
+               fname: firstName,
+               lname: lastName,
+               email: Email,
+               admin: true
+           }}
+       ).exec()
+       .then(()=>{
+           req.session.user = {
+            fname: firstName,
+            lname: lastName,
+            email: Email,
+            admin: true
+        };
+           res.redirect("/Profile");
+       });
+       
+   });
+   
+   /* #endregion */
+   
+
+
+
+
+
+
 
 
 // setup http server to listen on HTTP_PORT
